4e6d9d6346de102ba802152b437b964ac8a0857e
chitsanfei <emu-ws@outlook.com>
2024-02-06 16:25:23 +0800

    init

---
+++ config.ts
@@ -0,0 +1,25 @@
+export const opensource = {
+    active: [
+        'https://github.com/chitsanfei/rin-asf-bot',        
+      //'',
+    ],
+    toys: {
+      limit: 3,
+      random: true,
+      repos: [
+        'https://github.com/chitsanfei/koishi-plugin-setu',
+        //'',
+      ],
+    },
+  }
+  export const motto = `<p align=center><strong> Life will not give you any lemon. </strong></p>`
+  export const github = {
+    name: 'Chitsanfei',
+  }
+  
+  export const mxSpace = {
+    url: 'https://innei.ren',
+    api: 'https://api.innei.ren/v2',
+  }
+  
+  export const timeZone = 'Asia/Shanghai'
